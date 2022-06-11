de20ffc42d628c72d93e647b4e1dabc30b14733a
wheninseattle <weston.norwood@gmail.com>
2022-06-10 21:15:15 -0700

    update content

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,10 +9,23 @@ import { Shuksan } from "../assets/icons/Shuksan"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <div className={styles.textCenter}>
-      <h1>
-        Welcome <b>!</b>
-      </h1>
+    <div className="section">
+      <div className="welcome-content">
+        <h2 className="welcome">
+          Hi I'm,{" "}
+          <span style={{ color: "var(--color-primary)" }}>Weston Norwood</span>
+          .
+          <br />
+          Let's build something!
+        </h2>
+        <p>
+          A career in architecture kindled a passion for building tools that
+          solve problems, integrate technologies, and help people. I'm a new
+          software engineer driven to contriute to elegant solutions with user
+          experience as a guiding principal.. I thrive in diverse, collaborative
+          spaces, and I am fueled by curiosity and focused determination.
+        </p>
+      </div>
     </div>
     <div className="intro-graphic">
       <Shuksan className="intro-graphic" />
