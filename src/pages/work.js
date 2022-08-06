7ed11cab9d1351b8b22bd0ceff9a8c44fed252f1
Weston Norwood <weston.norwood@gmail.com>
2022-08-05 20:23:25 -0700

    feat: Update content

--- src/pages/work.js
+++ src/pages/work.js
@@ -1,21 +1,54 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import ProjectPanel from "../components/work/projectPanel"
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const WorkPage = () => {
+const WorkPage = ({data}) => {
+
+  const projects = data.projects.edges;
+  console.table(projects)
+
   return (
     <Layout>
       <Seo title="Work" />
       <div className="section">
         <div className="section-heading">Work</div>
-        <p>Coming soon!</p>
-        <ProjectPanel/>
+        {projects.map((project,i)=> {
+          return(
+          <ProjectPanel key={i} projectInfo={project.node}/>
+        )
+        })}
       </div>
     </Layout>
   )
 }
 
 export default WorkPage
+
+export const data = graphql`
+{
+  projects: allMarkdownRemark(
+    filter: { fileAbsolutePath: { regex: "/projects/" } }
+    sort: { fields: frontmatter___date, order: DESC }
+  ) {
+    edges {
+      node {
+        frontmatter {
+          date
+          name
+          activeTime
+          url
+          github
+          stack
+          featuredImage
+          description
+        }
+        html
+      }
+    }
+  }
+}
+`
