2fe6471e89ecaf9e4a4bddeefba7460cf0fbe9f1
Weston Norwood <weston.norwood@gmail.com>
2022-08-05 20:21:44 -0700

    feat: Add project panel

--- src/components/work/ProjectPanel.js
+++ src/components/work/ProjectPanel.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
+import { projectPanel,projectPanelRow, projectVideo,projectInformation,projectName,projectDescription,projectStack } from './work.module.css';
+import TestImage from '../../images/projects/DSC_2409.jpg';
+import FridgeVideo from '../../images/projects/CommunityFridgeMap.mp4';
+import { GatsbyImage } from 'gatsby-plugin-image';
+const ProjectPanel = ({ projectInfo }) => {
+    const { name, activeTime, url, github, stack, description, featuredImage } = projectInfo.frontmatter
 
-const ProjectPanel = () => {
     return (
-        <div>
-            <p>Testing</p>
+        <div className={featuredImage===''? 'projectPanel projectPanelRow' : 'project panel'}>
+            <div className={projectInformation}>
+                <div className={projectName}>
+                    {name}
+                </div>
+                <div className={projectStack}>
+                    {stack}
+                </div>
+                <br />
+                <div className={projectDescription}>
+                    {description}
+                </div>
+
+            </div>
+            {name === 'Community Fridges' ? <video className={projectVideo} autoplay muted loop controls>
+                <source src={FridgeVideo} type='video/mp4' />
+            </video> : null}
         </div>
     );
 }
 
-export default ProjectPanel;
+export default ProjectPanel;    
